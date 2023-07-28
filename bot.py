917c0fe908bde5c6d9e478515a9a44e058c7999b
Filmycloud1 <139132262+Filmycloud1@users.noreply.github.com>
2023-07-28 15:37:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "6419833706:AAGkIo2vhOpbhQZtWsgyGoFUaHMoaBEwyOA")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "28189122"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "1ebf75b3d12e23c93a79dab2aceb6eea")
 
 STRING = os.environ.get("STRING", "")
 
