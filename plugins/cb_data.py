3a8b690f462d81e36768023e032f93c3cf1327a5
Filmycloud1 <139132262+Filmycloud1@users.noreply.github.com>
2023-07-28 15:58:14 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -14,11 +14,11 @@ from helper.ffmpeg import take_screen_shot,fix_thumb
 from helper.progress import humanbytes
 from helper.set import escape_invalid_curly_brackets
 
-log_channel = int(os.environ.get("LOG_CHANNEL", ""))
+log_channel = int(os.environ.get("LOG_CHANNEL", "-1001934104589"))
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "26795065"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "02fdd0a1c8205dc41bea2f4b6784830a")
 
 STRING = os.environ.get("STRING", "")
 
